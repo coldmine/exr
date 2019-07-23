a571266998cd4901bf2d3b19da6b0cddd24bce95
kim yongbin <kybinz@gmail.com>
2019-07-23 16:33:34 +0900

    test: focus on scanline.exr

--- exr_test.go
+++ exr_test.go
@@ -8,8 +8,6 @@ func TestDecode(t *testing.T) {
 	// These are all valid exr files.
 	cases := []string{
 		"image/scanline.exr",
-		"image/singlepart.exr",
-		"image/multipart.exr",
 	}
 
 	for _, c := range cases {
